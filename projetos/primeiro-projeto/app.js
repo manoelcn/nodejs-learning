ef7f1bb174aa4239d0dde9ed387574ec601b2cae
manoel <candidomanoel78@gmail.com>
2025-05-05 21:12:55 -0300

    :sparkles: feat: Implementa rotas básicas no Express

---
+++ projetos/primeiro-projeto/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 8081;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>")
+});
+
+app.get("/sobre", (req, res) => {
+    res.send("<h1>Minha página sobre</h1>")
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}!`)
+}); // Sempre deverá ser a última linha do código!
