50bb8adcff912ce21e7f146b12ffc94c22fc7122
manoel <candidomanoel78@gmail.com>
2025-05-05 21:35:38 -0300

    :sparkles: feat: Adiciona rota com parâmetro no Express

--- projetos/primeiro-projeto/app.js
+++ projetos/primeiro-projeto/app.js
@@ -10,6 +10,10 @@ app.get("/sobre", (req, res) => {
     res.send("<h1>Minha página sobre</h1>")
 });
 
+app.get("/ola/:name", (req, res) => {
+    res.send(`<h1>Olá, ${req.params.name}!</h1>`)
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}!`)
 }); // Sempre deverá ser a última linha do código!
