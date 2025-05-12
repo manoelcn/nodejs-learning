036002e6166321c5f0489e523bc32295340f8846
manoel <candidomanoel78@gmail.com>
2025-05-12 11:44:32 -0300

    :sparkles: feat: Renderização de HTML no Express

--- projetos/primeiro-projeto/app.js
+++ projetos/primeiro-projeto/app.js
@@ -1,13 +1,18 @@
 import express from "express";
+import { fileURLToPath } from 'url'; // Converte URL 'file://' para caminho de arquivo
+import { dirname } from 'path'; // Extrai diretório de um caminho
+
 const app = express();
 const port = 8081;
+const __filename = fileURLToPath(import.meta.url); // Pega caminho completo do arquivo atual
+const __dirname = dirname(__filename); // Pega apenas a pasta do arquivo
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>")
+    res.sendFile(`${__dirname}/html/index.html`);
 });
 
 app.get("/sobre", (req, res) => {
-    res.send("<h1>Minha página sobre</h1>")
+    res.sendFile(`${__dirname}/html/sobre.html`);
 });
 
 app.get("/ola/:name", (req, res) => {
