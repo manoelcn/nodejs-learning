5d20295220ee478f84f8afe9ae20b33f7e111e79
manoel <candidomanoel78@gmail.com>
2025-06-14 12:14:31 -0300

    :wrench: chore: Configura template engine no projeto

--- projetos/cadastro-de-postagens/index.js
+++ projetos/cadastro-de-postagens/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import { Sequelize } from "sequelize";
+
+// Config
 
 const app = express();
 const port = 8082;
+
+// Template Engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+// Conexão com o bando de dados MySql
+const sequelize = new Sequelize('blog', 'root', '1234', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}!`)
