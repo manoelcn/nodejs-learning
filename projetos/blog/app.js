830ff6e4956794b5b72f8308790912bb31a2bce8
manoel <candidomanoel78@gmail.com>
2025-06-27 16:50:08 -0300

    :sparkles: feat: Registra rotas admin no app

--- projetos/blog/app.js
+++ projetos/blog/app.js
@@ -1,6 +1,7 @@
 // Importando módulos
 import express from 'express';
 import { engine } from "express-handlebars";
+import admin from './routes/admin.js';
 
 // Definindo constantes
 const app = express();
@@ -9,13 +10,13 @@ const PORT = 8081;
 //Configurações
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-    // Handlebars
-    app.engine('handlebars', engine());
-    app.set('view engine', 'handlebars');
-    app.set('views', './views');
+// Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 // Rotas
+app.use('/admin', admin);
 
 // Outros
 app.listen(PORT, () => {
