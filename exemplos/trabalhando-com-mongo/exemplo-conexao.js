a7aad53e2b8a98c41ffbbc6b27c56cce2c468f05
manoel <candidomanoel78@gmail.com>
2025-06-27 11:45:55 -0300

    :books: docs: Exemplo de model com mongoose

--- exemplos/trabalhando-com-mongo/exemplo-conexao.js
+++ exemplos/trabalhando-com-mongo/exemplo-conexao.js
@@ -3,7 +3,30 @@ const mongoose = require('mongoose');
 
 // Configurando o mongoose
 mongoose.connect('mongodb://127.0.0.1:27017/aprendendo').then(() => {
-    console.log('Conectado com sucesso!')
+    console.log('Conectado com sucesso!');
 }).catch((err) => {
     console.log(`Houve um erro ao se conectar ao mongoDB ${err}`);
+});
+
+// Definindo model de usuários
+const usuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
 });
