e71832bac9fe4afdb44bce235ec117a42f072ba8
manoel <candidomanoel78@gmail.com>
2025-06-30 11:34:31 -0300

    :sparkles: feat: Configura conexão com o banco de dados

--- projetos/blog/app.js
+++ projetos/blog/app.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import { engine } from "express-handlebars";
 import adminRouter from './routes/admin.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 // Definindo constantes
 const app = express();
@@ -10,12 +12,20 @@ const PORT = 8081;
 //Configurações
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
+    dotenv.config();
     // Handlebars
         app.engine('handlebars', engine());
         app.set('view engine', 'handlebars');
         app.set('views', './views');
     // Public
         app.use(express.static('public'));
+    // Mongoose
+        mongoose.Promise = global.Promise;
+        mongoose.connect(process.env.MONGO_URL).then(() => {
+            console.log('Conectado com sucesso!');
+        }).catch((err) => {
+            console.log(`Erro ao se conectar: ${err}`);
+        });
 
 // Rotas
 app.use('/admin', adminRouter);
