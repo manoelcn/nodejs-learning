dd08823419a3600b58ecd65345f4046a5723720a
manoel <candidomanoel78@gmail.com>
2025-06-30 11:35:13 -0300

    :sparkles: feat: Cria model de categoria

---
+++ projetos/blog/models/Categoria.js
@@ -0,0 +1,19 @@
+import mongoose, { mongo } from 'mongoose';
+const Schema = mongoose.Schema;
+
+const Categoria = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+mongo.model('categorias', Categoria);
