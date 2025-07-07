2804da39b84765258f696160cf32c560a0c33f33
manoel <candidomanoel78@gmail.com>
2025-07-07 12:12:35 -0300

    feat: Cria rota post para adicionar categorias ao bd

--- projetos/blog/routes/admin.js
+++ projetos/blog/routes/admin.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import Categoria from "../models/Categoria.js";
+
 const adminRouter = express.Router();
 
 adminRouter.get('/', (req, res) => {
@@ -15,6 +17,18 @@ adminRouter.get('/categorias', (req, res) => {
 
 adminRouter.get('/categorias/add', (req, res) => {
     res.render('admin/addcategorias');
+});
+
+adminRouter.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    };
+    new Categoria(novaCategoria).save().then(() => {
+        console.log('Categoria salva com sucesso!');
+    }).catch((err) => {
+        console.log(`Erro ao salvar categoria! ${err}`);
+    });
 });
 
 export default adminRouter;
